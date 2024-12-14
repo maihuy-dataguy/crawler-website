3360dbd67e42791652ad1e98014858667b51780f
Mai Huy <huym@rever.vn>
2024-12-15 01:14:40 +0700

    update clean code

--- exercise_1.py
+++ exercise_1.py
@@ -10,36 +10,37 @@ import pandas as pd
 category_ids = ['193870011', '172456', '16225007011']
 
 # Declare browser
-driver = webdriver.Chrome('chromedriver')
+driver = webdriver.Chrome()
 
 
-def extract_best_seller_product_info(link):
+def extract_best_seller_product_info(cat_id):
     # Open URL
+    link = 'https://www.amazon.com/gp/bestsellers/hi/{}'.format(cat_id)
     driver.get(link)
     sleep(random.randint(5, 10))
     # ================================Get rank
-    rank_elements = driver.find_elements(By.CSS_SELECTOR, ".zg-bdg-body")
+    rank_elements = driver.find_elements(By.CSS_SELECTOR, "div.zg-bdg-body")
     ranks = [elem.text for elem in rank_elements[:8]]
 
     # ==================================Get link/name
-    link_elements = driver.find_elements(By.CSS_SELECTOR, ".p13n-sc-uncoverable-faceout [tabindex]")
+    link_elements = driver.find_elements(By.CSS_SELECTOR, "div.p13n-sc-uncoverable-faceout > a[tabindex]")
     links = [elem.get_attribute('href') for elem in link_elements[:8]]
-    name_elements = driver.find_elements(By.CSS_SELECTOR, "._cDEzb_p13n-sc-css-line-clamp-3_g3dy1")
+    name_elements = driver.find_elements(By.CSS_SELECTOR, "div._cDEzb_p13n-sc-css-line-clamp-3_g3dy1")
     names = [elem.text for elem in name_elements[:8]]
     # ====================================Get price
-    price_elements = driver.find_elements(By.CSS_SELECTOR, ".p13n-sc-uncoverable-faceout .a-size-base")
+    price_elements = driver.find_elements(By.CSS_SELECTOR, "div.p13n-sc-uncoverable-faceout span.a-color-price")
     prices = [elem.text for elem in price_elements[:8]]
     # =====================================DataFrame
     df = pd.DataFrame(list(zip(ranks, names, prices, links)), columns=['rank', 'product_name', 'price', 'url'])
     df['price'] = df['price'].str[1:]
-    df.insert(0, "category_id", link.split('/')[-1])
+    df.insert(0, "category_id", cat_id)
     return df
 
 
 dfs = []
 
 for category_id in category_ids:
-    df = extract_best_seller_product_info('https://www.amazon.com/gp/bestsellers/hi/{}'.format(category_id))
+    df = extract_best_seller_product_info(category_id)
     dfs.append(df)
 
 result = pd.concat(dfs)
